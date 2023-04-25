edb3e3b043edd146492372819223caf47e703cf6
Ahmed Mokhtar <ahmedmokhtar7711@gmail.com>
2023-04-25 19:38:28 +0200

    new commit

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,8 +6,10 @@ class CalcTests(SimpleTestCase):
     def test_add_nimbers(self):
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
+
         
     def test_subtract_numbers(self):
         res = calc.subtract(5, 10)
         self.assertEqual(res, 5)
+
         
